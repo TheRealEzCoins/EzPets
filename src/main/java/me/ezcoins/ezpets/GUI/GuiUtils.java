b63a5c29eb78c7fb77dca8c9927990cad1593769
TheRealEzCoins <stevens.ian633@gmail.com>
2022-06-03 20:48:15 +0200

    v1.0.5 * Fixed pom file issue (I hope)

--- src/main/java/me/ezcoins/ezpets/GUI/GuiUtils.java
+++ src/main/java/me/ezcoins/ezpets/GUI/GuiUtils.java
@@ -1,40 +1,11 @@
 package me.ezcoins.ezpets.GUI;
 
 import org.bukkit.Material;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import static me.ezcoins.ezpets.Utils.customItemName;
-import static me.ezcoins.ezpets.Utils.makeItem;
 
 public class GuiUtils  {
-
-    public static ItemStack FILLER_GLASS = customItemName(Material.GRAY_STAINED_GLASS_PANE, " ");
-
-
-
-    public static void fillBorder(Inventory inventory) {
-        ItemStack item = FILLER_GLASS;
-        int size = inventory.getSize();
-        int rows = (size + 1) / 9;
-
-        // Fill top
-        for (int i = 0; i < 9; i++) {
-            inventory.setItem(i, item);
-        }
-
-        // Fill bottom
-        for (int i = size - 9; i < size; i++) {
-            inventory.setItem(i, item);
-        }
-
-        // Fill sides
-        for (int i = 2; i <= rows - 1; i++) {
-            int[] slots = new int[]{i * 9 - 1, (i - 1) * 9};
-            inventory.setItem(slots[0], item);
-            inventory.setItem(slots[1], item);
-        }
-    }
 
     public static ItemStack menuClose() {
         return customItemName(
--- src/main/java/me/ezcoins/ezpets/GUI/PetGUI.java
+++ src/main/java/me/ezcoins/ezpets/GUI/PetGUI.java
@@ -8,20 +8,31 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.inventory.ItemStack;
+
+import static me.ezcoins.ezpets.Utils.customItemName;
 
 public class PetGUI {
 
+    private static final ItemStack blackglass = customItemName(Material.BLACK_STAINED_GLASS_PANE, " ");
+
+    private static final int[] black_border = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            17, 18, 26, 27, 35, 36, 44, 46, 47, 48, 50,
+            51, 52, 53};
+
     public static Inventory PetGUI(Player player) {
         Inventory inventory = Bukkit.createInventory(null, 54, "Pets");
-        GuiUtils.fillBorder(inventory);
 
         inventory.setItem(49, GuiUtils.menuClose());
 
         if(Utils.hasPet(player, Pets.TESTPET)) {
-            inventory.setItem(10, Utils.customItemName(Material.PLAYER_HEAD, "§c"));
+            inventory.setItem(10, customItemName(Material.PLAYER_HEAD, "§c"));
         }
 
+        for (int slot : black_border)
+            inventory.setItem(slot, blackglass);
         return inventory;
+
     }
 }
